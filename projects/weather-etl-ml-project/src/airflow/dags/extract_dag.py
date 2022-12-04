5adb1acd866bf9b348051cabd245d1c5637377d7
kundigagandeep <kundgagandeep@gmail.com>
2022-12-04 02:25:31 +0100

    airflow updated

--- projects/weather-etl-ml-project/src/airflow/dags/extract_dag.py
+++ projects/weather-etl-ml-project/src/airflow/dags/extract_dag.py
@@ -1,8 +1,11 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-#from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-#from includes import extract_func, transform_func
+from airflow.decorators import task
+from pathlib import Path
+import requests
+import pandas as pd
+import logging
 
 
 default_args = {
@@ -13,16 +16,54 @@ default_args = {
     'email': ['airflow@airflow.com'],
     }
 
-with DAG('extract_dag',
-        default_args=default_args) as dag:
-        
-        task1 = BashOperator(
-        task_id='extract_weather_api',
-        bash_command="ls",
-        dag=dag)
 
-        # task2 = BashOperator(
-        # task_id='transform_data',
-        # bash_command="python includes/transform.py")
+with DAG('extract_dag', default_args=default_args, catchup=False) as dag:
+    
+    @task
+    def extract_func():
+        url = "https://weatherapi-com.p.rapidapi.com/current.json"
 
-        # task1.set_downstream(task2)
+        querystring = {"q":"Warsaw"}
+
+        headers = {
+                "X-RapidAPI-Key": "24eeab2ba1mshe19cfdccdd89090p1e1cc3jsnf0935a53f711",
+                "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        response_json = response.json()
+
+        df = pd.json_normalize(response_json)
+
+        for col in df.columns:
+            df.rename(columns={col:col.replace("location.","").replace("current.","")},inplace=True)
+
+        col_name = df['localtime_epoch'][0]
+
+        df.to_csv(f'/opt/airflow/dags/data/01_raw/{col_name}_weather_api.csv',index=False)
+
+        logging.info(f'Weather CSV created succesfully.')
+
+        p = Path("/opt/airflow/dags/data")
+
+        raw_data_path = p/'01_raw'
+        intermediate_data_path = p/'02_intermediate'
+
+        files = list(raw_data_path.iterdir())
+
+        df_main = []
+        for i in files:
+            df = pd.read_csv(i)
+            df_main.append(df)
+        df_final = pd.concat(df_main,axis=0)                 
+        df_final.to_csv(f'{str(intermediate_data_path)}/weather.csv', index=False)
+    
+    def print_now():
+        logging.info('WRITING NOW')
+        logging.info(datetime.utcnow().isoformat())
+        logging.info('TIME WRITTEN')
+
+    print_time_task = PythonOperator(task_id='print_now', python_callable=print_now, dag=dag)
+
+    print_time_task >> [extract_func()]
